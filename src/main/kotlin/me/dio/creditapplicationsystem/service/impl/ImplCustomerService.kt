002cf40b19672d605aad76f57f1c1adcfedc65f9
maiad <maiadiego290@gmail.com>
2024-02-18 13:44:31 -0300

    feat: add diegomaia04

---
+++ src/main/kotlin/me/dio/creditapplicationsystem/service/impl/ImplCustomerService.kt
@@ -0,0 +1,23 @@
+package me.dio.creditapplicationsystem.service
+
+import me.dio.creditapplicationsystem.entity.Customer
+import me.dio.creditapplicationsystem.repository.CustomerRepository
+import org.springframework.stereotype.Service
+
+@Service
+class implCustomerService(
+    private val customerRepository: CustomerRepository
+) : iCustomerService {
+
+    override fun save(customer: Customer): Customer =
+        this.customerRepository.save(customer)
+
+    override fun findById(id: Long): Customer =
+        this.customerRepository.findById(id).orElseThrow {
+            throw RuntimeException("id $id not found")
+        }
+
+    override fun delete(id: Long) =
+        this.customerRepository.deleteById(id)
+
+}
